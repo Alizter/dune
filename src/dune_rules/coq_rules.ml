1d9c0c487639f0aae883ea8aec3251d78084c971
Ali Caglayan <alizter@gmail.com>
2022-11-19 17:51:07 +0100

    refactor(coq): resolve theories and theories_deps earlier
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/dune_rules/coq_rules.ml
+++ src/dune_rules/coq_rules.ml
@@ -457,7 +457,6 @@ let source_rule ~sctx theories =
      tree to produce correct dependencies, including those of dependencies *)
   Action_builder.dyn_paths_unit
     (let open Action_builder.O in
-    let* theories = Resolve.Memo.read theories in
     let+ l =
       Action_builder.List.map theories ~f:(coq_modules_of_theory ~sctx)
     in
@@ -601,6 +600,8 @@ let setup_theory_rules ~sctx ~dir ~dir_contents (s : Theory.t) =
       and+ theories = theories_deps in
       theory :: theories
     in
+    let open Action_builder.O in
+    let* theories = Resolve.Memo.read theories in
     source_rule ~sctx theories
   in
   let* mode = select_native_mode ~sctx ~dir s.buildable in
@@ -761,9 +762,10 @@ let setup_extraction_rules ~sctx ~dir ~dir_contents (s : Extraction.t) =
     theories_deps_requires_for_user_written ~dir s.buildable
   in
   let source_rule =
-    let theories = source_rule ~sctx theories_deps in
     let open Action_builder.O in
-    theories >>> Action_builder.path (Path.build (Coq_module.source coq_module))
+    let* theories_deps = Resolve.Memo.read theories_deps in
+    source_rule ~sctx theories_deps
+    >>> Action_builder.path (Path.build (Coq_module.source coq_module))
   in
   let* mode = select_native_mode ~sctx ~dir s.buildable in
   setup_coqdep_and_coqc_rule cctx ~dir ~sctx ~loc:s.buildable.loc
