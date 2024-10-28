2a63dc0919ede3e7ba9ad513019c1eee952bae7a
Ali Caglayan <alizter@gmail.com>
2024-10-28 14:22:42 +0000

    feature: add aliases for inline tests
    For a given library with inlines tests, we add an alias with the name of
    the library for running those tests.
    
    Further work on separating out the partition actions would allow us to
    have an alias for each partition.
    
    For now, this allows us to run inline tests with the runtest command.
    
    Signed-off-by: Ali Caglayan <alizter@gmail.com>

--- src/dune_rules/inline_tests.ml
+++ src/dune_rules/inline_tests.ml
@@ -348,16 +348,25 @@ include Sub_system.Register_end_point (struct
             |> Action_builder.with_stdout_to partition_file
             |> Super_context.add_rule sctx ~dir ~loc
         in
-        let* runtest_alias =
-          match mode with
-          | Native | Best | Byte -> Memo.return Alias0.runtest
-          | Jsoo mode -> Jsoo_rules.js_of_ocaml_runtest_alias ~dir ~mode
+        let alias =
+          Lib_name.Local.to_string lib_name |> Alias.Name.of_string |> Alias.make ~dir
+        in
+        let* () =
+          let* runtest_alias =
+            (match mode with
+             | Native | Best | Byte -> Memo.return Alias0.runtest
+             | Jsoo mode -> Jsoo_rules.js_of_ocaml_runtest_alias ~dir ~mode)
+            >>| Alias.make ~dir
+          in
+          Dep.alias alias
+          |> Action_builder.dep
+          |> Rules.Produce.Alias.add_deps runtest_alias ~loc
         in
         Super_context.add_alias_action
           sctx
           ~dir
           ~loc:info.loc
-          (Alias.make ~dir runtest_alias)
+          alias
           (let open Action_builder.O in
            let+ actions =
              let* partitions_flags =
